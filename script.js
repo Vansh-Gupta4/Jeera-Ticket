aca7f6257f8794b96a9e9782500929da379beecc
Vansh Gupta <67005624+Vansh-Gupta4@users.noreply.github.com>
2021-05-06 21:35:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let filterColor = document.querySelectorAll(".filter");
 let mainContainer = document.querySelector(".main-container");
+let modalContainer = document.querySelector(".modal_container");
+let taskBox = document.querySelector(".task_box");
+let modalFlag = false;
 let plusBtn = document.querySelector(".plus");
+let iColor="black";
 
 for (let i = 0; i < filterColor.length; i++) {
     filterColor[i].addEventListener("click", function () {
@@ -16,17 +20,28 @@ for (let i = 0; i < filterColor.length; i++) {
     })
 }
 plusBtn.addEventListener("click", function () {
-    let task = prompt("Enter Yout Task");
-    let color = prompt("Color");
-    if (task != "" && color != "") {
+    // let task = prompt("Enter Yout Task");
+    // let color = prompt("Color");
+
+   modalContainer.style.display="flex";
+    
+})
+
+taskBox.addEventListener("keydown",function(e){
+    
+    if (e.key == "Enter" && taskBox.value != "") {
         let taskContainer = document.createElement("div");  //creates div
+        let task=taskBox.value;
         taskContainer.setAttribute("class", "ticket_container"); 
-        taskContainer.innerHTML = `<div class="ticket_color ${color}"></div>   
+        taskContainer.innerHTML = `<div class="ticket_color ${iColor}"></div>   
              <div class="ticket_desc_container">
                  <div class="ticket_id">#ExampleId</div>
                  <div class="ticket_desc">${task}</div>
              </div>`;  //.innerHTML adds html
              mainContainer.appendChild(taskContainer);
+
+            //cleanup code
+             modalContainer.style.display="none";
+             taskBox.value="";
     }
-    // console.log(ans);
 })
