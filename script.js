587aebd8b84afabc45dc2c027069afb5cb4a2adb
Vansh Gupta <67005624+Vansh-Gupta4@users.noreply.github.com>
2021-05-07 02:11:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let filterColor = document.querySelectorAll(".filter");
 let mainContainer = document.querySelector(".main-container");
+let modalColors = document.querySelectorAll(".modal-color");
 let modalContainer = document.querySelector(".modal_container");
 let taskBox = document.querySelector(".task_box");
 let modalFlag = false;
 let plusBtn = document.querySelector(".plus");
 let iColor="black";
+let colors=["pink","blue","green","black"];
 
 for (let i = 0; i < filterColor.length; i++) {
     filterColor[i].addEventListener("click", function () {
@@ -43,5 +45,30 @@ taskBox.addEventListener("keydown",function(e){
             //cleanup code
              modalContainer.style.display="none";
              taskBox.value="";
+             iColor="black";
+             addFunctionality(taskContainer);
     }
 })
+
+for(let i=0;i<modalColors.length;i++){
+    modalColors[i].addEventListener("click",function(){
+        let color=modalColors[i].classList[1];
+        iColor=color;
+        for(let j=0;j<modalColors.length;j++){
+            modalColors[j].classList.remove("border");
+        }
+        modalColors[i].classList.add("border");
+    })
+}
+
+function addFunctionality(taskContainer){
+    let ticketColor=taskContainer.querySelector(".ticket_color");
+    ticketColor.addEventListener("click",function(){
+        let cColor=ticketColor.classList[1];
+        let idx=colors.indexOf(cColor);
+        let newIdx=(idx+1)%4;
+        let newColor=colors[newIdx];
+        ticketColor.classList.remove(cColor);
+        ticketColor.classList.add(newColor);
+    })
+}
