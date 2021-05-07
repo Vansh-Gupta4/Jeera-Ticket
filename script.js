1ed8c059e37d19e63c45ced4556910cd9b823e54
Vansh Gupta <67005624+Vansh-Gupta4@users.noreply.github.com>
2021-05-07 16:34:40 +0530

    Added comments

--- script.js
+++ script.js
@@ -1,76 +1,147 @@
-let filterColor = document.querySelectorAll(".filter");   //querySelectorAll h ..therefore array milega
-let mainContainer = document.querySelector(".main-container");
-let modalColors = document.querySelectorAll(".modal-color");   //querySelectorAll h ..therefore array milega
-let modalContainer = document.querySelector(".modal_container");
-let taskBox = document.querySelector(".task_box");
-let modalFlag = false;
-let plusBtn = document.querySelector(".plus");
-let iColor="black";   //initial color is black
-let colors=["pink","blue","green","black"];
+body{
+   margin:0px;
+   padding:0px;
+   font-family: 'Roboto Slab', serif;
 
-for (let i = 0; i < filterColor.length; i++) {
-    filterColor[i].addEventListener("click", function () {
-        let classes = filterColor[i].getAttribute("class");
-        // console.log(classes);
-        let strArr = classes.split(" ");
-        let color = strArr[1];
-        let mainClasses = mainContainer.getAttribute("class");
-        let mainCArr = mainClasses.split(" ");
-        mainCArr[1] = color;
-        mainClasses = mainCArr.join(" ");
-        mainContainer.setAttribute("class", mainClasses);
-    })
 }
-plusBtn.addEventListener("click", function () {
-    // let task = prompt("Enter Yout Task");
-    // let color = prompt("Color");
+*{
+   box-sizing: border-box;
+   /* border:1px solid red; */
+}
+.pink{
+background-color:lightpink;
+}
+.blue{
+   background-color:lightblue;
+}
+.green{
+   background-color:lightgreen;
+}
+.black{
+   background-color:rgb(33, 31, 31);
+}
+.toolbar{
+   display: flex;     /* dono children same line mai aa gye */
+   height: 12vh;
+   background-color: rgb(32, 29, 29);
+   align-items: center;  /* beech mai le aaya*/
+}
+.filter_container{
+   height: 70%;   /*70% of 12vh*/
+   background-color: darkgray;
+   width: 20rem;
+   border-radius: 3px;
+   margin-right: 2rem;
+   margin-left: 2rem;
+   display: flex;   /* chaaro children same line mai aa gye */
+} 
+.filter_color-container{
+   width: 25%;
+   height: 100%;
+   background: #444;
+   border-right: 1px solid red;
+   /* perefectly center */
+   display: flex;
+   justify-content: center;
+   align-items: center;
+}
+.filter_color{
+   height: 40%;
+   width:40%;
+   border-radius: 2px ;
+}
+.action_container{
+   height: 70%;    /*70% of 12vh*/
+   background-color: darkgray;
+   width: 8rem;
+   border-radius: 3px;
+   display: flex;    /*  children same line mai aa gye */
+}
+.icon_container{
+   height: 100%;
+   width: 50%;
+   border-right: 1px solid red;
 
-   modalContainer.style.display="flex";   //jab "+" par click toh modal container visible ho jayega
-    
-})
-
-taskBox.addEventListener("keydown",function(e){    //agar text area par click karenge toh fiunction chalega 
-    
-    if (e.key == "Enter" && taskBox.value != "") {
-        let taskContainer = document.createElement("div");  //creates div
-        let task=taskBox.value;
-        taskContainer.setAttribute("class", "ticket_container"); 
-        taskContainer.innerHTML = `<div class="ticket_color ${iColor}"></div>   
-             <div class="ticket_desc_container">
-                 <div class="ticket_id">#ExampleId</div>
-                 <div class="ticket_desc">${task}</div>
-             </div>`;  //.innerHTML adds html
-             mainContainer.appendChild(taskContainer);
-
-            //cleanup code
-             modalContainer.style.display="none";   //modal invisble ho jayega
-             taskBox.value="";    //ab next time par appear hoga toh textarea khaali milega
-             iColor="black";
-             addFunctionality(taskContainer);  
-    }
-})
-
-for(let i=0; i<modalColors.length; i++){
-    modalColors[i].addEventListener("click", function(){   //agar kisi bhi color par click ho toh function chalega
-        let color=modalColors[i].classList[1];   //classList=>uss element par jitni class h h voh de deta h (example => "modal-color pink" mai se humne "pink" choose kiya)
-        iColor=color;
-        for(let j=0;j<modalColors.length;j++){
-            //remove "border" class from every color
-            modalColors[j].classList.remove("border");   //remove function aisa h jiss par "border" class lagi h usse remove kar dega aur jispar nhi lagi usko kuch nhi bolega (err nhi dega)
-        }
-        modalColors[i].classList.add("border");   //jis par click kiya uss par "border" class add kar dega
-    })
+   /* Trick to make content at perfectly center */
+   display: flex;
+   justify-content: center;
+   align-items: center;
+}
+.icon{
+   font-size:1.5rem;
+}
+.main-container{
+   min-height: 88vh;
+   padding-top: 3rem;
+   padding-left: 3rem;
+   background-color:aliceblue;
+   display: flex;
+   justify-content: space-around;
+   flex-wrap: wrap;
+}
+.ticket_container{
+   /* border: 1px solid red; */
+   height: 10rem;
+   width: 14rem;
+   background-color: white;
+}
+.ticket_color{   /*ticket ke upar color strip ka selector*/
+   height: 7%;
+}
+.ticket_desc_container{
+   height: 93%;
+   padding: 0.5rem;
+   overflow: auto;
+   word-break: break-all;
 }
 
-//ticket bane ke baad uska color change karne ka function
-function addFunctionality(taskContainer){
-    let ticketColor=taskContainer.querySelector(".ticket_color");  //ticket ka present color ka selector mil jayega
-    ticketColor.addEventListener("click",function(){   //uss selector par click hua toh function chalega
-        let cColor=ticketColor.classList[1];   //isse current color mil jayega
-        let idx=colors.indexOf(cColor);   //indexOf function bata dega ki voh present color konse index par h
-        let newIdx=(idx+1)%4;   //circular array bann jayega
-        let newColor=colors[newIdx];   //new color mil jayega
-        ticketColor.classList.remove(cColor);  //phele wale color ko remove kiya
-        ticketColor.classList.add(newColor);   //aur new color ko set kar dia
-    })
+.ticket_id{
+   margin-bottom: 11px;
+   font-weight: 800;
+   color:lightslategray;
+   
+}
+.ticket_desc{
+   text-align: center;
+   font-size: 1.2rem;
+}
+/* modal container */
+.modal_container{
+  /* border:solid 1px red; */
+  height: 18rem;
+  width: 31rem;
+  display: flex;  /*kyuki children humme ek ke baad ek chaiye*/
+  position: absolute;
+  box-shadow: 0 1rem 5rem rgba(0,0,0,0.637);
+  display: none; /* "none" karne se dikhega nhi*/
+}
+.task_container{
+   /* border:1px solid green; */
+   width: 76%;
+   height: 100%;
+}
+.modal_color-container{
+   /* border: 1px solid green; */
+   width: 24%;
+   height: 100%;
+   background-color: #444;
+}
+.task_box{
+   height: 100%;
+   width: 100%;
+   padding: 2rem;
+}
+.modal_color-container{
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: space-evenly;
+}
+.modal-color{
+   height: 12%;
+   width: 73%;
+   border-radius: 2px;
+}
+.border{
+   border: 5px solid black;
 }
